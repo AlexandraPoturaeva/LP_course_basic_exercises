8d1b34c1dc32a941d386b661674de0ccceb58db7
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-05-19 18:11:47 +0300

    string_challenges

--- string_challenges.py
+++ string_challenges.py
@@ -1,16 +1,23 @@
 # Вывести последнюю букву в слове
 word = 'Архангельск'
-# ???
+print(word[-1])
 
 
 # Вывести количество букв "а" в слове
 word = 'Архангельск'
-# ???
+word = word.lower()
+print(word.count('а'))
 
 
 # Вывести количество гласных букв в слове
+rus_vowels = 'аоуэыяёеюи'
 word = 'Архангельск'
-# ???
+word_vowels_cnt = 0
+for letter in word.lower():
+    if letter in rus_vowels:
+        word_vowels_cnt += 1
+print(f'Количество гласных букв в слове "{word}": {word_vowels_cnt}')
+
 
 
 # Вывести количество слов в предложении
@@ -20,9 +27,11 @@ sentence = 'Мы приехали в гости'
 
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
-# ???
+print(len(sentence.split()))
 
 
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
-# ???
+words_lengths = [len(word) for word in sentence.split()]
+avg_word_length = sum(words_lengths) / len(words_lengths)
+print(avg_word_length)
